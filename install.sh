913d1c52a82ea24b3e0436e02310a5aac3066385
Calos Kao <calos3257@gmail.com>
2019-03-11 00:18:16 +0800

    init

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+BASEDIR=$(dirname "$0")
+
+if [ ! -d ~/.env ]; then
+    echo "Copy files into ~/.env"
+    mkdir ~/.env
+    cp -rp $BASEDIR/* ~/.env/
+fi
+BASEDIR=~/.env
+
+if [ -f ~/.minttyrc ]; then
+    echo "Backup exists file ~/.minttyrc"
+    mv ~/.minttyrc ~/.minttyrc.bak.$(date +%Y%m%d%H%M%S)
+fi
+
+if [ ! -L ~/.minttyrc ]; then
+    echo "Create link ~/.minttyrc"
+    ln -s $BASEDIR/minttyrc ~/.minttyrc
+fi
+
+if [ ! -f ~/.zshrc ]; then
+    echo "Create file ~/.zshrc"
+    touch ~/.zshrc
+fi
+
+src_cmd="source $(eval echo ~/.env/source_map.sh)"
+if ! grep -Fxq "$src_cmd" ~/.zshrc; then
+    echo "Install source map in ~/.zshrc"
+    echo "$src_cmd" >> ~/.zshrc
+fi
+
+echo "Done."
---
+++ source_map.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+
+BASEDIR=$(dirname "$0")
+
+source $BASEDIR/aliases
