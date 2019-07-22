d607462dcb68f9ebf7d8ffba096ac88445e7f602
Calos Kao <calos.kao@ttc.org.tw>
2019-07-22 15:03:22 +0800

    Auto reload .zshrc after installation.

--- install.sh
+++ install.sh
@@ -1,33 +1,37 @@
-#!/bin/sh
+#!env zsh
 
 BASEDIR=$(dirname "$0")
 
-if [ ! -d ~/.env ]; then
-    echo "Copy files into ~/.env"
-    mkdir ~/.env
-    cp -rp $BASEDIR/* ~/.env/
+if [ ! -d $HOME/.env ]; then
+    echo "Copy files into $HOME/.env"
+    mkdir $HOME/.env
+    cp -rp $BASEDIR/* $HOME/.env/
 fi
-BASEDIR=~/.env
+BASEDIR=$HOME/.env
 
-if [ -f ~/.minttyrc ]; then
-    echo "Backup exists file: ~/.minttyrc"
-    mv ~/.minttyrc ~/.minttyrc.bak.$(date +%Y%m%d%H%M%S)
-fi
-
-if [ ! -L ~/.minttyrc ]; then
-    echo "Create link: ~/.minttyrc"
-    ln -s $BASEDIR/minttyrc ~/.minttyrc
+if [ -f $HOME/.minttyrc ]; then
+    echo "Backup exists file: $HOME/.minttyrc"
+    mv $HOME/.minttyrc $HOME/.minttyrc.bak.$(date +%Y%m%d%H%M%S)
 fi
 
-if [ ! -f ~/.zshrc ]; then
-    echo "Create file: ~/.zshrc"
-    touch ~/.zshrc
+if [ ! -L $HOME/.minttyrc ]; then
+    echo "Create link: $HOME/.minttyrc"
+    ln -s $BASEDIR/minttyrc $HOME/.minttyrc
 fi
 
-src_cmd="source $(eval echo ~/.env/source_map.sh)"
-if ! grep -Fxq "$src_cmd" ~/.zshrc; then
-    echo "Add source map in ~/.zshrc"
-    echo "$src_cmd" >> ~/.zshrc
+if [ ! -f $HOME/.zshrc ]; then
+    echo "Create file: $HOME/.zshrc"
+    touch $HOME/.zshrc
 fi
+
+src_cmd="source $(eval echo $HOME/.env/source_map.sh)"
+if ! grep -Fxq "$src_cmd" $HOME/.zshrc; then
+    echo "Add source map in $HOME/.zshrc"
+    echo "$src_cmd" >> $HOME/.zshrc
+fi
+
+printf "Reloading $HOME/.zshrc ... "
+
+source $HOME/.zshrc
 
 echo "Done."
