607d9cce7f4c3b24a24b13e2ee9da09edd032a1a
Calos.Kao <calos.kao@ttc.org.tw>
2023-03-25 15:56:51 +0800

    Update install command.

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
+echo "##### Begin install caloskao/env #####"
+
 sudo apt update && sudo apt install -y zsh;
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
-# sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="risto"/' ~/.zshrc;
-# sed -i 's/plugins=(git)/plugins=(git zsh_reload)/' ~/.zshrc;
-# echo 'export TERM=xterm' >> ~/.zshrc;
-echo 'HIST_STAMPS="yyyy-mm-dd"' >> ~/.zshrc;
-git clone https://github.com/caloskao/env ~/.env;
-cat ~/.env/lib-inst/*.sh | sh;
-source ~/.zshrc;
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended;
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
-zsh -c "omz theme set powerlevel10k/powerlevel10k";
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ${ZSH_CUSTOM:-$HOME/.zshrc};
+
+# echo 'export TERM=xterm' >> ~/.zshrc;
+
+echo 'HIST_STAMPS="yyyy-mm-dd"' >> $HOME/.zshrc;
+
+git clone https://github.com/caloskao/env $HOME/.env;
+
+cat $HOME/.env/lib-inst/*.sh | sh;
+
+zsh -c "source $HOME/.zshrc";
+
+echo "##### Installation completed! Enjoy! #####";
