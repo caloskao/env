f336eb958151028970faf4d3538088094d268424
Calos.Kao <calos.kao@ttc.org.tw>
2024-06-11 15:09:45 +0800

    Add aliases.

--- inc/zsh_set_term_title.sh
+++ inc/zsh_set_term_title.sh
@@ -5,8 +5,14 @@ function __zsh_set_term_title() {
     title $1
 }
 
+function __zsh_reset_term_title() {
+    DISABLE_AUTO_TITLE=false
+}
+
 alias zsh-set-term-title=__zsh_set_term_title
 alias set-term-title=__zsh_set_term_title
+alias zsh-reset-term-title=__zsh_reset_term_title
+alias reset-term-title=__zsh_reset_term_title
 
 # if [ ! type set-term-title ] then
 #     alias set-term-title="__zsh_set_term_title"
